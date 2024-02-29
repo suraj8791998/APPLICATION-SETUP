0d9b5f3e55f6b46171d6c2ff6e34fcecf66c7f36
hemasuraj879 <your_email@example.com>
2024-02-29 15:56:52 +0530

    files pushing

--- ansible.sh
+++ ansible.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
 sudo yum install ansible -y
+if [ $? -ne 0];
+then
+    echo "INSTALLATION ANSIBLE IS SUCCES"
+else
+    echo "INSTALLATION ANSIBLE IS FAILURE"
+fi 
 sudo dnf install ansible-core -y
 
+if [ $? -ne 0];
+then
+    echo "INSTALLATION ANSIBLE CORE IS SUCCES"
+else
+    echo "INSTALLATION ANSIBLE CORE IS FAILURE"
+fi 
+
+cd /tmp
 git clone https://github.com/suraj8791998/JENKINS-INSTALLATION.git
 
-cd JENKINS-INSTALLATION
+if [ $? -ne 0];
+then
+    echo "GIT CLONING IS SUCCES"
+else
+    echo "GIT CLONING IS FAILURE"
+fi 
+
+cd /tmp/JENKINS-INSTALLATION
 ansible-playbook jenkins.yaml 
