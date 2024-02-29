f19b0a1c6498e32b10fba88f353d685b1e6c3a4b
hemasuraj879 <your_email@example.com>
2024-02-29 15:49:52 +0530

    files pushing

--- ansible.sh
+++ ansible.sh
@@ -3,9 +3,6 @@
 sudo yum install ansible -y
 sudo dnf install ansible-core -y
 
-
-mkdir /ansible
-cd /ansible
 git clone https://github.com/suraj8791998/JENKINS-INSTALLATION.git
 
 cd JENKINS-INSTALLATION
